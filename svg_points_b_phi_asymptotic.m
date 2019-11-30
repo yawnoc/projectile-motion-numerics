0388ac0b496fb7bdcc2b7d5e072f6a3d20823d4e
yawnoc <leeconway@protonmail.com>
2019-11-30 18:51:19 +0800

    Points for B vs asymptotic optimal phi SVG

---
+++ svg_points_b_phi_asymptotic.m
@@ -0,0 +1,12 @@
+b_big_values = (0 : 0.05 : 2.05)';
+phi_pd_opt_values = optimal_angle_asymptotic (b_big_values);
+
+## Scale B and negate phi for SVG
+b_big_values *= 4;
+phi_pd_opt_values *= -1;
+
+csvwrite (
+  "svg-points-b-phi-asymptotic.csv",
+  [b_big_values, phi_pd_opt_values],
+  "precision", 4
+);
