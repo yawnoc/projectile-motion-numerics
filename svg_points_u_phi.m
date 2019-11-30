845cebfa5b9f98432f45d598341aa71583f03da6
yawnoc <leeconway@protonmail.com>
2019-11-30 19:07:55 +0800

    Points for u/c vs optimal phi SVG

---
+++ svg_points_u_phi.m
@@ -0,0 +1,13 @@
+u_per_c_values = (0 : 0.1 : 4)';
+b_big_values = u_per_c_values .^ 2;
+phi_pd_opt_values = optimal_angle (b_big_values);
+
+## Scale u/c and negate phi for SVG
+u_per_c_values *= 5;
+phi_pd_opt_values *= -1;
+
+csvwrite (
+  "svg-points-u-phi.csv",
+  [u_per_c_values, phi_pd_opt_values],
+  "precision", 4
+);
