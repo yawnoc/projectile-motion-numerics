f9c1fbe29ed5a7b2f9561e447f3484636843f2db
yawnoc <leeconway@protonmail.com>
2019-11-30 18:05:47 +0800

    B vs optimal phi table

---
+++ table_b_phi.m
@@ -0,0 +1,27 @@
+b_big_values = [ ...
+  0 : 0.1 : 1, ...
+  2 : 1 : 10, ...
+  15, 20, 50, 100, 200, 500, ...
+  10 .^ [3, 4, 6, 8]
+]';
+
+phi_pd_opt_values = optimal_angle (b_big_values);
+
+csvwrite (
+  "table-b-phi.csv",
+  [b_big_values, phi_pd_opt_values],
+  "precision", 3
+);
+
+
+small_b_indices = b_big_values <= 2;
+b_big_values = b_big_values(small_b_indices);
+phi_pd_opt_values = phi_pd_opt_values(small_b_indices);
+phi_pd_opt_asy_values = optimal_angle_asymptotic (b_big_values);
+percent_error_values = (phi_pd_opt_asy_values ./ phi_pd_opt_values - 1) * 100;
+
+csvwrite (
+  "table-b-phi-asymptotic.csv",
+  [b_big_values, phi_pd_opt_asy_values, percent_error_values],
+  "precision", 3
+);
